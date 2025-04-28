52f1c7b4e6b65fd2e005d8ab7fdf08e8363d56e2
nicolevito <coemakeup@gmail.com>
2025-04-28 14:26:19 -0300

    django-forms

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
