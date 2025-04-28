13a28be616cd3b2f7a1b1c2e700076391c820d46
nicolevito <coemakeup@gmail.com>
2025-04-28 11:22:35 -0300

    Atualização

---
+++ mysite/blog/forms.py
@@ -0,0 +1,7 @@
+from blog.models.comment import Comment
+from django import forms
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ("name",  "email", "body")
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blog import views
+from blog.views import post_detail
+from . import views
 
 urlpatterns = [
     path('home/', views.PostViews.as_view(), name='home'), 
-    path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
+    path("<slug:slug>/", post_detail, name="post_detail"),
 ]
